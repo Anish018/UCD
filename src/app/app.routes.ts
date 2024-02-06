0684e44d4f0549be213a2f50f1590e4e043a25a6
Anish <anish.n1809@gmail.com>
2024-02-06 14:52:33 +0530

    second

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component'; 
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '', component: LandingComponent },
+    {path: 'privacy-policy', component: PrivacyPolicyComponent }
+
+];
